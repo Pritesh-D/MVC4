c751ceb5175b79b37ecdad5f881b6b85cf8b6d6f
[Pritesh] <[pdhokchaule@tavisca.com]>
2015-11-20 15:15:08 +0530

    initial commit

---
+++ MvcBasicDemo/MvcBasicDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBasicDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "It Works";
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Please Login Here";
+            return View();
+        }
+    }
+}
